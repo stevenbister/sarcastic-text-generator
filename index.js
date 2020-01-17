f81bc1953669c07cf0d2dda0f053bd8b0c7cbb35
Steven Bister (University of Birmingham Conferences and Events) <s.bister@bham.ac.uk>
2020-01-17 14:15:19 +0000

    Created functions to capitalise every second letter from the text area and output it to a p

---
+++ index.js
@@ -0,0 +1,19 @@
+const textarea = document.querySelector('[name=textarea]');
+const output = document.querySelector('.output')
+
+const makeStringSarcastic = (str) => {
+  // split the string into an array
+  const arr = [... str];
+
+  // Get every second letter
+  const uppercaseEverySecondLetter = arr.map( (letter, index) => {
+    // If number is divisable by 2 make it uppercase
+    // Else make it lowecase
+    return ( index % 2 ) ? letter.toUpperCase() : letter.toLowerCase();
+  })
+
+  // Join the text back together again
+  return uppercaseEverySecondLetter.join('')
+}
+
+textarea.addEventListener('input', e => output.textContent = makeStringSarcastic(e.target.value) )
